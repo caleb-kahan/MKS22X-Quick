c65af38187cc9757fead45eeb3ffe23106233ee8
Caleb Kahan <ckahan00@stuy.edu>
2019-03-11 14:51:03 -0400

    starting

---
+++ Quick.java
@@ -0,0 +1,17 @@
+public class Quick{
+  public static int quickselect(int []data, int k){
+
+   }
+   int partition ( int [] data, int start, int end){
+     int difference = end - start + 1;
+     int add = (int)(Math.random()*difference);
+     int pivot = start+add;
+
+   }
+   private int [] rearrange(int [] data, int pivot){
+     int value = data[pivot];
+     for(int i=0;i<data.length;i++){
+       if(data[i]>value && )
+     }
+   }
+ }
