af32c1c2a6880f214b9cc6a79d7ca2ab144ab1e9
Caleb Kahan <ckahan00@stuy.edu>
2019-03-11 22:46:50 -0400

    fillin in the array for the next stage

--- Quick.java
+++ Quick.java
@@ -1,8 +1,21 @@
 public class Quick{
   public static int quickselect(int []data, int k){
-    return 0;
+    int pivot = partition(data,0,data.length);
+    while(pivot!=k){
+      if(pivot>k){
+        int size = data.length-pivot-1;
+        int [] returner = new int[size];
+        for(int i = data.length-1, j=0;i>data.length-1-size;i--,j++){
+          returner[j]=data[i];
+
+        }
+      }
+        pivot = partition(data,0,data.length);
+    }
+    return data[pivot];
+
    }
-   public int partition ( int [] data, int start, int end){
+    public static int partition ( int [] data, int start, int end){
      if(data==0){
        int difference = end - start + 1;
        int add = (int)(Math.random()*difference);
@@ -26,7 +39,7 @@ public class Quick{
      }
      return start-1;
    }
-   public int [] swap(int [] data, int index1, int index2){
+   public static int [] swap(int [] data, int index1, int index2){
      int temp = data[index1];
      data[index1]=data[index2];
      data[index2]=temp;
