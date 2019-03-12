f50fef789ed4c3ef94c4d853e6307a399f8fc4b0
Caleb Kahan <ckahan00@stuy.edu>
2019-03-11 21:17:05 -0400

    i don't think im supposed to do a for loop

--- Quick.java
+++ Quick.java
@@ -1,17 +1,18 @@
 public class Quick{
   public static int quickselect(int []data, int k){
-
+    return 0;
    }
    int partition ( int [] data, int start, int end){
      int difference = end - start + 1;
      int add = (int)(Math.random()*difference);
      int pivot = start+add;
-
-   }
-   private int [] rearrange(int [] data, int pivot){
-     int value = data[pivot];
-     for(int i=0;i<data.length;i++){
-       if(data[i]>value && )
+     for(int i=start;i<end;i++){
+       if(data[i]>data[pivot] && pivot>i){
+         int temp = data[i];
+         data[i]=data[pivot];
+         data[pivot]=temp;
+       }
      }
+     return pivot;
    }
  }
