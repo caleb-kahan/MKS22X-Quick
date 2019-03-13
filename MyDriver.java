685f8f95d7757178ed44d98d86bee4e85291c5c4
Caleb Kahan <ckahan00@stuy.edu>
2019-03-12 23:01:43 -0400

    partition works perfectly

--- MyDriver.java
+++ MyDriver.java
@@ -1,5 +1,10 @@
 public class MyDriver{
   public static void main(String [] args){
-    
+    int [] myBest = {3,5,9,4,3,9,2,1,6,4,7,8,6,8,6};
+    System.out.println(Quick.partition(myBest,0,myBest.length-1));
+    System.out.println();
+    for(int x: myBest){
+      System.out.println(x);
+    }
   }
 }
