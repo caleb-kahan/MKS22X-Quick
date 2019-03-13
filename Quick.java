d726928b627cba29feb9e35d0bad83de8ab91712
Caleb Kahan <ckahan00@stuy.edu>
2019-03-13 01:38:21 +0000

    i plant to do finish partition and quickselect

--- Quick.java
+++ Quick.java
@@ -23,28 +23,31 @@ public class Quick{
 
    }
     public static int partition ( int [] data, int start, int end){
-     if(start==0){
-       int difference = end - start + 1;
-       int add = (int)(Math.random()*difference);
-       int pivot = start+add;
-       data = swap(data,start,pivot);
-       return partition(data,start+1,end);
-     }
-     int temp = start;
-     while(start<end && data[start]<=data[0]) start++;
-     while(end>start && data[end]>data[0]) end--;
-     for(int i=start;i<=end;i++){
-       if(data[i]<=data[0]){
-         data = swap(data, i, start);
-         return partition(data,start+1,end);
-       }
+     if(start<end){
+     	if(start==0){
+       		int difference = end - start + 1;
+       		int add = (int)(Math.random()*difference);
+       		int pivot = start+add;
+       		data = swap(data,start,pivot);
+       		return partition(data,start+1,end);
+     	}
+     	int temp = start;
+     	while(start<end && data[start]<=data[0]) start++;
+     	while(end>start && data[end]>data[0]) end--;
+     	for(int i=start;i<=end;i++){
+      		if(data[i]<=data[0]){
+       	  		data = swap(data, i, start);
+         		return partition(data,start+1,end);
+       		}
+     	}
      }
      //Everthing's done at this point, woohoo
-     boolean swapWithStart = data[0]>=data[start];
+     boolean swapWithStart = data[0]<data[start];
      if(swapWithStart){
-       return start;
+
+       return start-1;
      }
-     return start-1;
+     return start;
    }
    public static int [] swap(int [] data, int index1, int index2){
      int temp = data[index1];
