5b540ce271e0da740644292ed5955f3d8c3fbd80
Caleb Kahan <ckahan00@stuy.edu>
2019-03-17 05:03:44 +0000

    1000 aint work

--- Quick.java
+++ Quick.java
@@ -37,7 +37,7 @@ public class Quick{
       quicksort(data,pivot+1,end);
   }*/
   public static void quicksort(int[] data, int start, int end) {
-      if(end-start<5000){
+      if(end-start<1000){
 	  insertionSort(data,start,end);
 	  return;
       }
